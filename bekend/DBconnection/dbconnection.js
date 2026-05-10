c944d8a70f51bcdac6def1eb72dbaf1fa85b5f16
teadjo <teodoradjogovic101@gmail.com>
2026-05-10 15:19:26 +0200

    preusmjeravanje na server bazu

--- bekend/DBconnection/dbconnection.js
+++ bekend/DBconnection/dbconnection.js
@@ -1,11 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: "postgres",
-    host: "127.0.0.1",
-    database: "UmjetnickoTakmicenje",
-    password: "password",
-    port: "5432",
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
