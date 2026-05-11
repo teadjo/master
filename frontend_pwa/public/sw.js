495b403235d01a49f314edbbbbc9a44bf9967e5d
teadjo <teodoradjogovic101@gmail.com>
2026-05-11 20:07:32 +0200

    update putanja za vercel

--- frontend_pwa/public/sw.js
+++ frontend_pwa/public/sw.js
@@ -35,7 +35,7 @@ console.log('✅ Custom SW učitan');
 
 registerRoute(
   ({ url }) =>
-    url.origin === 'https://master-azure-two.vercel.app/' &&
+    url.origin.includes('vercel.app') &&
     url.pathname.startsWith('/uploads'),
 
   new CacheFirst({
@@ -123,7 +123,7 @@ registerRoute(
 
 registerRoute(
   ({ url, request }) =>
-    url.origin === 'https://master-azure-two.vercel.app/' &&
+    url.origin.includes('vercel.app') &&
     request.method === 'GET',
 
     new StaleWhileRevalidate({
@@ -280,7 +280,7 @@ const postBgSync = new BackgroundSyncPlugin('postQueue', {
 
 registerRoute(
   ({ url, request }) =>
-    url.origin === 'https://master-azure-two.vercel.app/' &&
+    url.origin.includes('vercel.app') &&
     request.method === 'POST',
 
   new NetworkOnly({
@@ -304,7 +304,7 @@ const mutateBgSync = new BackgroundSyncPlugin('mutateQueue', {
 
 registerRoute(
   ({ url, request }) =>
-    url.origin === 'https://master-azure-two.vercel.app/' &&
+    url.origin.includes('vercel.app') &&
     ['PUT', 'PATCH', 'DELETE'].includes(request.method),
 
   new NetworkOnly({
