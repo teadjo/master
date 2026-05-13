83b19958025a39a64dcdd01017119432284e36b5
teadjo <teodoradjogovic101@gmail.com>
2026-05-13 17:44:03 +0200

    critical css

--- frontend_pwa/vite.config.js
+++ frontend_pwa/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
-
+import criticalCSS from 'vite-plugin-critical-css';
 export default defineConfig(() => ({
   plugins: [
     react(),
