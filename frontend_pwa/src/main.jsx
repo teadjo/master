cab3995c424d6d9991d64fa3c36a13e5080c8413
teadjo <teodoradjogovic101@gmail.com>
2026-05-23 23:20:35 +0200

    uklonjene font awesome

--- frontend_pwa/src/main.jsx
+++ frontend_pwa/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import '@fortawesome/fontawesome-free/css/all.min.css';
 import { ToastProvider } from './ToastContext.jsx';
 import { onCLS, onLCP, onTTFB, onINP } from 'web-vitals';
 import { pwaMetrics } from './utils/metrics';
