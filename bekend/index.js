001a3403f1f5ad879266435f2e5532a45fed51ad
teadjo <teodoradjogovic101@gmail.com>
2026-05-24 13:37:02 +0200

    cors

--- bekend/index.js
+++ bekend/index.js
@@ -23,7 +23,14 @@ if (!fs.existsSync(uploadDir)) {
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://master-spa2.vercel.app',
+    'https://master-azure-two.vercel.app',
+    'http://localhost:5173'
+  ],
+  credentials: true
+}));
 app.use(compression());
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads'), {
