286f2d5d0fafd5bb494c0d58649b4e5ba5550fc0
teadjo <teodoradjogovic101@gmail.com>
2026-09-14 13:40:48 +0200

    prebacivanje nevironment varijabli na localhost jer sam ugasila render(bekend i bazu)

--- bekend/DBconnection/dbconnection.js
+++ bekend/DBconnection/dbconnection.js
@@ -1,10 +1,22 @@
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: process.env.NODE_ENV === 'production' 
+//     ? { rejectUnauthorized: false } 
+//     : false   // Lokalno isključi SSL
+// });
+
+// module.exports = pool;
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' 
-    ? { rejectUnauthorized: false } 
-    : false   // Lokalno isključi SSL
+    user: "postgres",
+    host: "127.0.0.1",
+    database: "UmjetnickoTakmicenje",
+    password: "password",
+    port: "5432",
 });
 
 module.exports = pool;
